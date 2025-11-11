a11e6710f7736c068c67c42b5cb4690841532766
migekkoHecker <116096246+migekkoHecker@users.noreply.github.com>
2025-11-11 11:59:57 +0100

    Update data.js

--- slimey-adventure/data.js
+++ slimey-adventure/data.js
@@ -9,21 +9,9 @@ const CHARACTERS = {
     color: "#77dd77",
 
     actions: {
-      action1: {
-        name: "Slime",
-        attack: "1d4",
-        range: 2
-      },
-      special: {
-        name: "Slimey shield",
-        heal: 2,
-        shield: 3,
-        cooldown: 4
-      },
-      movement: {
-        name: "Move",
-        move: "1d4"
-      }
+      action1: { name: "Slime", attack: "1d4", range: 2 },
+      special: { name: "Slimey shield", heal: 2, shield: 3, cooldown: 4 },
+      movement: { name: "Move", move: "1d4" }
     }
   },
 
@@ -36,23 +24,9 @@ const CHARACTERS = {
     color: "#0077b6",
 
     actions: {
-      action1: {
-        name: "Sword",
-        attack: "1d6 +1"
-      },
-      special: {
-        name: "Swift slash",
-        attack1: "1d4",
-        move1: 2,
-        attack2: "1d4 -1",
-        move2: 2,
-        attack3: "1d4 -2",
-        cooldown: 5
-      },
-      movement: {
-        name: "Move",
-        move: "1d4"
-      }
+      action1: { name: "Sword", attack: "1d6 +1" },
+      special: { name: "Swift slash", attack1: "1d4", move1: 2, attack2: "1d4 -1", move2: 2, attack3: "1d4 -2", cooldown: 5 },
+      movement: { name: "Move", move: "1d4" }
     }
   },
 
@@ -65,23 +39,9 @@ const CHARACTERS = {
     color: "#e3b505",
 
     actions: {
-      action1: {
-        name: "Bow",
-        attack: "1d6",
-        range: 4
-      },
-      special: {
-        name: "Multishot",
-        attack1: "1d4",
-        range: 3,
-        target: 3,
-        cooldown: 4
-      },
-      movement: {
-        name: "Move",
-        move: 3,
-        heal: "1d4 -2"
-      }
+      action1: { name: "Bow", attack: "1d6", range: 4 },
+      special: { name: "Multishot", attack1: "1d4", range: 3, target: 3, cooldown: 4 },
+      movement: { name: "Move", move: 3 }
     }
   }
 };
@@ -91,27 +51,20 @@ const MAPS = {
   meadow: {
     name: "Meadow Plains",
     size: { w: 7, h: 5 },
-
     logic: [
       [0,0,0,0,0,0,0],
       [0,1,0,1,0,1,0],
       [0,0,0,0,0,0,0],
       [0,1,0,1,0,1,0],
-      [0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0]
     ],
-
     art: [
       [0,0,0,0,0,0,0],
       [0,2,0,2,0,2,0],
       [0,0,0,0,0,0,0],
       [0,2,0,2,0,2,0],
-      [0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0]
     ],
-
-    artLegend: {
-      0: "grass",
-      1: "stone",
-      2: "wall"
-    }
+    artLegend: { 0: "grass", 1: "stone", 2: "wall" }
   }
 };
