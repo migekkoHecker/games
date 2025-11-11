747f3499a8b08fab582f5d6df2886c289489f0e6
migekkoHecker <116096246+migekkoHecker@users.noreply.github.com>
2025-11-11 11:13:05 +0100

    Update data.js

--- slimey-adventure/data.js
+++ slimey-adventure/data.js
@@ -10,20 +10,22 @@ const CHARACTERS = {
     int: "1d20",
     color: "#77dd77",
 
-    baseActions: {
-      attack: { type: "attack", value: "1d4", range: 2 },
+    actions: {
+      action1: {
+        name: "Slime",
+        attack: "1d4",
+        range: 2
+      },
       special: {
         name: "Slimey shield",
         heal: 2,
         shield: 3,
         cooldown: 4
       },
-      movement: { move: "1d4" }
-    },
-
-    abilities: {
-      stickySlime: { name: "Sticky slime", slow: 1, range: 2 },
-      speedySlime: { name: "Speedy slime", move: 4 }
+      movement: {
+        name: "Move",
+        move: "1d4"
+      }
     }
   },
 
@@ -35,25 +37,24 @@ const CHARACTERS = {
     int: "1d20 -4",
     color: "#0077b6",
 
-    baseActions: {
-      sword: { type: "attack", value: "1d6 +1" },
+    actions: {
+      action1: {
+        name: "Sword",
+        attack: "1d6 +1"
+      },
       special: {
         name: "Swift slash",
-        sequence: [
-          { attack: "1d4" },
-          { move: 2 },
-          { attack: "1d4 -1" },
-          { move: 2 },
-          { attack: "1d4 -2" }
-        ],
+        attack1: "1d4",
+        move1: 2,
+        attack2: "1d4 -1",
+        move2: 2,
+        attack3: "1d4 -2",
         cooldown: 5
       },
-      movement: { move: "1d4" }
-    },
-
-    abilities: {
-      shield: { name: "Shield", shield: 2 },
-      sharpSword: { name: "Sharp sword", attackBonus: 2 }
+      movement: {
+        name: "Move",
+        move: "1d4"
+      }
     }
   },
 
@@ -65,24 +66,28 @@ const CHARACTERS = {
     int: "1d6 +10",
     color: "#e3b505",
 
-    baseActions: {
-      bow: { type: "attack", value: "1d6", range: 4 },
+    actions: {
+      action1: {
+        name: "Bow",
+        attack: "1d6",
+        range: 4
+      },
       special: {
         name: "Multishot",
-        attack: "1d4",
+        attack1: "1d4",
         range: 3,
         target: 3,
         cooldown: 4
       },
-      movement: { move: 3, heal: "1d4 -2" }
-    },
-
-    abilities: {
-      focus: { name: "Focus", rangeBonus: 1 },
-      medkit: { name: "Medkit", heal: "1d4 +1" }
+      movement: {
+        name: "Move",
+        move: 3,
+        heal: "1d4 -2"
+      }
     }
   }
 };
+
 
 // === MAPS ===
 const MAPS = {
@@ -90,7 +95,6 @@ const MAPS = {
     name: "Meadow Plains",
     size: { w: 7, h: 5 },
 
-    // logic grid (0 = walkable, 1 = wall)
     logic: [
       [0,0,0,0,0,0,0],
       [0,1,0,1,0,1,0],
@@ -99,7 +103,6 @@ const MAPS = {
       [0,0,0,0,0,0,0],
     ],
 
-    // art grid (for rendering visuals)
     art: [
       [0,0,0,0,0,0,0],
       [0,2,0,2,0,2,0],
